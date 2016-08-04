e46feefb3e48a7d92f243552341d2d49bb6c0d82
Nick <nkrass@users.noreply.github.com>
2016-08-04 20:53:12 +0700

    Update to support class

--- index.js
+++ index.js
@@ -3,198 +3,184 @@
  * Module dependencies.
  */
 
-var Emitter = require('events').EventEmitter;
-var parser = require('socket.io-parser');
+const Emitter = require('events').EventEmitter;
+const parser = require('socket.io-parser');
 
 /**
- * Module exports.
+ * Room constructor.
+ * @type {{new(): {add: (function(String)), del: (function(String))}}}
+ * @api private
+ * @private
  */
+const Room = class {
+  constructor(){
+    //if (!(this instanceof Room)) return new Room();
+    this.sockets = {};
+    this.length = 0;
+  }
 
-module.exports = Adapter;
+  /**
+   * Adds a socket to a room.
+   *
+   * @param {String} id socket id
+   * @api private
+   */
+  add(id){
+    if (!this.sockets.hasOwnProperty(id)) {
+      this.sockets[id] = true;
+      this.length++;
+    }
+  }
+  /**
+   * Removes a socket from a room.
+   *
+   * @param {String} id socket id
+   * @api private
+   */
+  del(id){
+    if (this.sockets.hasOwnProperty(id)) {
+      delete this.sockets[id];
+      this.length--;
+    }
+  }
+}
 
 /**
  * Memory adapter constructor.
- *
- * @param {Namespace} nsp
+ * @constructor
+ * @param {String} nsp
  * @api public
+ * @export
  */
 
-function Adapter(nsp){
-  this.nsp = nsp;
-  this.rooms = {};
-  this.sids = {};
-  this.encoder = new parser.Encoder();
-}
-
-/**
- * Inherits from `EventEmitter`.
- */
-
-Adapter.prototype.__proto__ = Emitter.prototype;
-
-/**
- * Adds a socket to a room.
- *
- * @param {String} socket id
- * @param {String} room name
- * @param {Function} callback
- * @api public
- */
-
-Adapter.prototype.add = function(id, room, fn){
-  this.sids[id] = this.sids[id] || {};
-  this.sids[id][room] = true;
-  this.rooms[room] = this.rooms[room] || Room();
-  this.rooms[room].add(id);
-  if (fn) process.nextTick(fn.bind(null, null));
-};
-
-/**
- * Removes a socket from a room.
- *
- * @param {String} socket id
- * @param {String} room name
- * @param {Function} callback
- * @api public
- */
-
-Adapter.prototype.del = function(id, room, fn){
-  this.sids[id] = this.sids[id] || {};
-  delete this.sids[id][room];
-  if (this.rooms.hasOwnProperty(room)) {
-    this.rooms[room].del(id);
-    if (this.rooms[room].length === 0) delete this.rooms[room];
+const Adapter = class extends Emitter{
+  constructor(nsp){
+    super();
+    this.nsp = nsp;
+    this.rooms = {};
+    this.sids = {};
+    this.encoder = new parser.Encoder();
   }
-
-  if (fn) process.nextTick(fn.bind(null, null));
-};
-
-/**
- * Removes a socket from all rooms it's joined.
- *
- * @param {String} socket id
- * @param {Function} callback
- * @api public
- */
-
-Adapter.prototype.delAll = function(id, fn){
-  if (this.sids[id]) {
-    Object.keys(this.sids[id]).filter( room => this.rooms[room] )
-      .map( room => {
-          this.rooms[room].del(id);
-          if (this.rooms[room].length === 0) delete this.rooms[room];
-      })  
+  /**
+   * Adds a socket to a room.
+   *
+   * @param {String} id socket id
+   * @param {String} room name
+   * @param {Function} fn callback
+   * @api public
+   */
+  add(id, room, fn){
+    this.sids[id] = this.sids[id] || {};
+    this.sids[id][room] = true;
+    this.rooms[room] = this.rooms[room] || new Room();
+    this.rooms[room].add(id);
+    if (fn) process.nextTick(fn.bind(null, null));
   }
-  delete this.sids[id];
+  /**
+   * Removes a socket from a room.
+   *
+   * @param {String} id socket id
+   * @param {String} room name
+   * @param {Function} fn callback
+   * @api public
+   */
+  del(id, room, fn){
+    this.sids[id] = this.sids[id] || {};
+    delete this.sids[id][room];
+    if (this.rooms.hasOwnProperty(room)) {
+      this.rooms[room].del(id);
+      if (this.rooms[room].length === 0) delete this.rooms[room];
+    }
 
-  if (fn) process.nextTick(fn.bind(null, null));
-};
+    if (fn) process.nextTick(fn.bind(null, null));
+  }
+  /**
+   * Removes a socket from all rooms it's joined.
+   *
+   * @param {String} id socket id
+   * @param {Function} fn callback
+   * @api public
+   */
+  delAll(id, fn){
+    if (this.sids[id]) {
+      Object.keys(this.sids[id]).filter( room => this.rooms[room] )
+          .map( room => {
+            this.rooms[room].del(id);
+            if (this.rooms[room].length === 0) delete this.rooms[room];
+          })
+    }
+    delete this.sids[id];
 
-/**
- * Broadcasts a packet.
- *
- * Options:
- *  - `flags` {Object} flags for this packet
- *  - `except` {Array} sids that should be excluded
- *  - `rooms` {Array} list of rooms to broadcast to
- *
- * @param {Object} packet object
- * @api public
- */
+    if (fn) process.nextTick(fn.bind(null, null));
+  }
+  /**
+   * Broadcasts a packet.
+   *
+   * Options:
+   *  - `flags` {Object} flags for this packet
+   *  - `except` {Array} sids that should be excluded
+   *  - `rooms` {Array} list of rooms to broadcast to
+   *
+   * @param {Object} packet object
+   * @param {{rooms: Array, flags: Object, except: Array}} opts
+   * @api public
+   */
+  broadcast(packet = {}, opts = {}){
+    var rooms = opts.rooms || [];
+    var except = opts.except || [];
+    var flags = opts.flags || {};
+    var packetOpts = {
+      preEncoded: true,
+      volatile: flags.volatile,
+      compress: flags.compress
+    };
+    packet.nsp = this.nsp.name;
 
-Adapter.prototype.broadcast = function(packet, opts){
-  var rooms = opts.rooms || [];
-  var except = opts.except || [];
-  var flags = opts.flags || {};
-  var packetOpts = {
-    preEncoded: true,
-    volatile: flags.volatile,
-    compress: flags.compress
-  };
-  packet.nsp = this.nsp.name;
-  
-  this.encoder.encode(packet, (encodedPackets) => {
-    if (rooms.length) {
+    this.encoder.encode(packet, (encodedPackets) => {
+      if (rooms.length) {
         rooms.filter( room => this.rooms[room] )
-          .map( room => this.rooms[room] )
+            .map( room => this.rooms[room] )
+            .map( room => Object.keys(room.sockets) )
+            .join().split(',')
+            .filter( id => !except.includes(id) )
+            .map( id => this.nsp.connected[id] )
+            .filter( e => e)
+            .map( sc => sc.packet(encodedPackets, packetOpts) );
+      } else {
+        Object.keys(this.sids).filter( id => !except.includes(id) )
+            .map( id => this.nsp.connected[id] )
+            .filter( e => e )
+            .map( sc => sc.packet(encodedPackets, packetOpts) );
+      }
+    });
+  }
+  /**
+   * Gets a list of clients by sid.
+   *
+   * @param {Array} rooms explicit set of rooms to check.
+   * @param {Function} fn callback
+   * @api public
+   */
+  clients(rooms = [], fn){
+    // if ('function' === typeof rooms){
+    //   fn = rooms;
+    //   rooms = null;
+    // }
+    if (!fn) return;
+    let sids = [];
+
+    if (rooms.length) {
+      sids = rooms.filter( room => this.rooms[room] )
           .map( room => Object.keys(room.sockets) )
           .join().split(',')
-          .filter( id => !except.includes(id) )
-          .map( id => this.nsp.connected[id] )
-          .filter( e => e)
-          .map( sc => sc.packet(encodedPackets, packetOpts) );
+          .filter( sc => this.nsp.connected[sc]);
     } else {
-      Object.keys(this.sids).filter( id => !except.includes(id) )
-        .map( id => this.nsp.connected[id] )
-        .filter( e => e )
-        .map( sc => sc.packet(encodedPackets, packetOpts) );
-    }
-  });
-};
-
-/**
- * Gets a list of clients by sid.
- *
- * @param {Array} explicit set of rooms to check.
- * @api public
- */
-
-Adapter.prototype.clients = function(rooms, fn){
-  if ('function' === typeof rooms){
-    fn = rooms;
-    rooms = null;
-  }
-  if (!fn) return;
-  var sids = [];
-  rooms = rooms || [];
-  
-  if (rooms.length) {
-      sids = rooms.filter( room => this.rooms[room] )
-        .map( room => Object.keys(room.sockets) )
-        .join().split(',')
-        .filter( sc => this.nsp.connected[sc]);
-  } else {
       sids = Object.keys(this.sids).filter( sc => this.nsp.connected[sc]);
+    }
+    process.nextTick(fn.bind(null, null, sids));
   }
-  process.nextTick(fn.bind(null, null, sids));
-};
-
-/**
-* Room constructor.
-*
-* @api private
-*/
-
-function Room(){
-  if (!(this instanceof Room)) return new Room();
-  this.sockets = {};
-  this.length = 0;
 }
-
 /**
- * Adds a socket to a room.
- *
- * @param {String} socket id
- * @api private
+ * Module exports.
  */
-
-Room.prototype.add = function(id){
-  if (!this.sockets.hasOwnProperty(id)) {
-    this.sockets[id] = true;
-    this.length++;
-  }
-};
-
-/**
- * Removes a socket from a room.
- *
- * @param {String} socket id
- * @api private
- */
-
-Room.prototype.del = function(id){
-  if (this.sockets.hasOwnProperty(id)) {
-    delete this.sockets[id];
-    this.length--;
-  }
-};
+module.exports = Adapter;
